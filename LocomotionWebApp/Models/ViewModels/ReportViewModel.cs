7fe60cea98d54388a706efe8154ae39e94ef3f7d
Kate Rilett <katerilett@gmail.com>
2013-06-21 13:41:21 -0500

    Added nonfunctional Edit button to required inputs table. Added values to ReportViewModel

--- LocomotionWebApp/Models/ViewModels/ReportViewModel.cs
+++ LocomotionWebApp/Models/ViewModels/ReportViewModel.cs
@@ -11,6 +11,16 @@ namespace LocomotionWebApp.Models.ViewModels
 	{
 		public long ID { get; set; }
 
+		public String FirstName { get; set; }
+
+		public String LastName { get; set; }
+
+		public DateTime LastUpdate { get; set; }
+
+		public int ShankLength { get; set; }
+
+		public int ThighLength { get; set; }
+
 		public Report Report { get; set; }
 	}
 }
